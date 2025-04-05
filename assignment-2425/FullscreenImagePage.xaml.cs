1dadfa824cc02be7a71f1ae7a558c6b2c0c78577
Pablo24z <josephalison2000@gmail.com>
2025-04-05 13:25:24 +0100

    Added functioning Carousel for menu front and back, also adjusted the fullscreeenimagepage in order to provide a cleaner transition and functioning back nav

--- assignment-2425/FullscreenImagePage.xaml.cs
+++ assignment-2425/FullscreenImagePage.xaml.cs
@@ -3,24 +3,26 @@ namespace assignment_2425;
 [QueryProperty(nameof(ImageSource), "ImageSource")]
 public partial class FullscreenImagePage : ContentPage
 {
+    public string ImageSource
+    {
+        set => FullImage.Source = value;
+    }
+
     public FullscreenImagePage()
     {
         InitializeComponent();
     }
 
-    private ImageSource _imageSource;
-    public ImageSource ImageSource
-    {
-        get => _imageSource;
-        set
-        {
-            _imageSource = value;
-            FullImage.Source = value;
-        }
-    }
-
     private async void CloseImage(object sender, EventArgs e)
     {
-        await Shell.Current.GoToAsync("..");
+        try
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that may occur during navigation
+            await DisplayAlert("Error", ex.Message, "OK");
+        }
     }
 }
--- assignment-2425/MenuPage.xaml.cs
+++ assignment-2425/MenuPage.xaml.cs
@@ -4,16 +4,16 @@ namespace assignment_2425;
 
 public partial class MenuPage : ContentPage
 {
-	public ObservableCollection<string> MenuImages { get; set; }
+	public ObservableCollection<MenuImage> MenuImages { get; set; }
 	public MenuPage()
 	{
 		InitializeComponent();
 
-		MenuImages = new ObservableCollection<string>
+		MenuImages = new ObservableCollection<MenuImage>
 		{
-			"menus/casablanca_menufront.png",
-			"menus/casablanca_menuback.png"
-		};
+			new MenuImage { ImageSource = "menus/casablanca_menufront.png" },
+            new MenuImage { ImageSource = "menus/casablanca_menuback.png" }
+        };
 
 		BindingContext = this;
 
@@ -21,14 +21,19 @@ public partial class MenuPage : ContentPage
 
 	private async void OnImageTapped(object sender, EventArgs e)
 	{
-		if (sender is Image image && image.Source != null)
+		if (sender is Image image && image.Source is FileImageSource fileSource)
 		{
-			await Shell.Current.GoToAsync("FullscreenImagePage", true, new Dictionary<string, object>
+			await Shell.Current.GoToAsync("///FullscreenImagePage", new Dictionary<string, object>
 			{
 				{
-					"ImageSource", image.Source
+					"ImageSource", fileSource.File
 				}
 			});
 		}
 	}
+}
+
+public class MenuImage
+{
+    public string ImageSource { get; set; }
 }
