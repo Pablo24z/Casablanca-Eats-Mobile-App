38ca7fe5646e9ecf58f1e83cb0aaef1b01881132
Pablo24z <josephalison2000@gmail.com>
2025-04-08 09:08:29 +0100

    Changed the nav bar to have icon for dark mode and also changed cs behind it so based on screen size it creates a hamburger menu with a dropdown as icons and buttons did not have enough space without overlapping

--- assignment-2425/StickyNavBar.xaml.cs
+++ assignment-2425/StickyNavBar.xaml.cs
@@ -4,32 +4,54 @@ namespace assignment_2425;
 
 public partial class StickyNavBar : ContentView
 {
-	public StickyNavBar()
-	{
-		InitializeComponent();
-	}
-	private async void OnLogoTapped(object sender, EventArgs e)
+    private bool _isDarkMode = false;
+    private bool _isFlyoutVisible = false;
+
+    public StickyNavBar()
     {
-        // Handle the event
+        InitializeComponent();
+    }
+
+    private async void OnLogoTapped(object sender, EventArgs e)
+    {
         await Shell.Current.GoToAsync("//MainPage");
     }
-	private async void OnMenuClicked(object sender, EventArgs e)
+
+    private async void OnMenuClicked(object sender, EventArgs e)
     {
-        // Handle the event
         await Shell.Current.GoToAsync("//MenuPage");
+        ToggleFlyout(false);
     }
 
     private async void OnContactClicked(object sender, EventArgs e)
     {
-        // Handle the event
         await Shell.Current.GoToAsync("//ContactPage");
+        ToggleFlyout(false);
     }
 
-	private async void OnOrderClicked(object sender, EventArgs e)
+    private async void OnOrderClicked(object sender, EventArgs e)
     {
-		// Handle the event
-		await Shell.Current.GoToAsync("//OrderPage");
+        await Shell.Current.GoToAsync("//OrderPage");
+        ToggleFlyout(false);
     }
 
+    private void OnHamburgerClicked(object sender, EventArgs e)
+    {
+        _isFlyoutVisible = !_isFlyoutVisible;
+        FlyoutMenu.IsVisible = _isFlyoutVisible;
+    }
 
+    private void ToggleFlyout(bool visible)
+    {
+        _isFlyoutVisible = visible;
+        FlyoutMenu.IsVisible = visible;
+    }
+
+    private void OnToggleThemeClicked(object sender, EventArgs e)
+    {
+        _isDarkMode = !_isDarkMode;
+        App.Current.UserAppTheme = _isDarkMode ? AppTheme.Dark : AppTheme.Light;
+
+        ThemeToggleButton.Source = _isDarkMode ? "sun_icon.svg" : "moon_icon.svg";
+    }
 }
