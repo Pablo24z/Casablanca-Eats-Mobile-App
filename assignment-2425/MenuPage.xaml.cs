0c85533de9e5ee37725e686e7ad3fedaaa7e71c7
Pablo24z <josephalison2000@gmail.com>
2025-04-11 03:39:54 +0100

    Update to the MenuPage, should have fixed menu and added popular dishes icon

--- assignment-2425/MenuPage.xaml.cs
+++ assignment-2425/MenuPage.xaml.cs
@@ -4,36 +4,39 @@ namespace assignment_2425;
 
 public partial class MenuPage : ContentPage
 {
-	public ObservableCollection<MenuImage> MenuImages { get; set; }
-	public MenuPage()
-	{
-		InitializeComponent();
+    public ObservableCollection<MenuImage> MenuImages { get; set; }
+    public MenuPage()
+    {
+        InitializeComponent();
 
-		MenuImages = new ObservableCollection<MenuImage>
-		{
-			new MenuImage { ImageSource = "menus/casablanca_menufront.png" },
+        MenuImages = new ObservableCollection<MenuImage>
+        {
+            new MenuImage { ImageSource = "menus/casablanca_menufront.png" },
             new MenuImage { ImageSource = "menus/casablanca_menuback.png" }
         };
 
-		BindingContext = this;
+        BindingContext = this;
 
-	}
+    }
 
-	private async void OnImageTapped(object sender, EventArgs e)
-	{
-		if (sender is Image image && image.Source is FileImageSource fileSource)
-		{
-			await Shell.Current.GoToAsync("///FullscreenImagePage", new Dictionary<string, object>
-			{
-				{
-					"ImageSource", fileSource.File
-				}
-			});
-		}
-	}
+    private async void OnImageTapped(object sender, EventArgs e)
+    {
+        if (sender is Image image && image.Source != null)
+        {
+            string imagePath = (image.Source as FileImageSource)?.File;
+
+            if (!string.IsNullOrEmpty(imagePath))
+            {
+                await Shell.Current.GoToAsync("///FullscreenImagePage", new Dictionary<string, object>
+            {
+                { "ImageSource", imagePath }
+            });
+            }
+        }
+    }
 }
 
-public class MenuImage
+    public class MenuImage
 {
     public string ImageSource { get; set; }
 }
